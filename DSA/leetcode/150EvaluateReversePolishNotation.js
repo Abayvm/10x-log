42c8c4bc3ae6effb25809ea7141da38b18129ea1
Abayvm <abayvmuraleedharan@gmail.com>
2025-08-03 22:07:17 +0530

    more optimal solution without using Map

--- DSA/leetcode/150EvaluateReversePolishNotation.js
+++ DSA/leetcode/150EvaluateReversePolishNotation.js
@@ -22,3 +22,37 @@ var evalRPN = function(tokens) {
     }
     return stack[0];
 };
+
+//more optimal solution without using Map
+
+var evalRPN = function(tokens) {
+    let stack = [];
+
+    for(let i=0; i<tokens.length; i++){
+        if(tokens[i]=="+"){
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+            let res = num1+num2;
+            stack.push(res);
+        }else if(tokens[i]=="*"){
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+            let res = num1*num2;
+            stack.push(res);
+        }else if(tokens[i]=="-"){
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+            let res = num2-num1;
+            stack.push(res);
+        }else if(tokens[i]=="/"){
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+            let res = Math.trunc(num2 / num1);
+            stack.push(res);
+        }else{
+            let num = Number(tokens[i]);
+            stack.push(num);
+        }
+    }
+    return stack[0];
+};
